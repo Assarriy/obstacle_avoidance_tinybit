f3484e8a5ea2bcf2feab48f5edd27d03184a5f1c
Assariy <113223089+Assarriy@users.noreply.github.com>
2023-03-07 14:05:56 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,57 @@
+let Distance = 0
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    Distance = Tinybit.Ultrasonic_Car()
+    if (Distance < 15) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, 100)
+        basic.pause(100)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, 100)
+        basic.pause(100)
+    } else {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 100)
+    }
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+})
