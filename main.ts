2415797f3abbe6186af1063f6da7f4c67d099519
Wibowo Assariy <113223089+Assarriy@users.noreply.github.com>
2023-11-17 09:22:10 +0700

    Fix

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ basic.forever(function () {
     if (Distance < 15) {
         Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, 100)
         basic.pause(100)
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, 100)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, 100)
         basic.pause(100)
     } else {
         Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 100)
